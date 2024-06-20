d9e5699c8199d05799ae8d9eb5263f08a55c255c
realseongho <qotjdgh000@gmail.com>
2024-06-20 18:47:18 +0900

    2

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120840
-function solution(balls, share) {
-  let i,
-    top = 1,
-    bot = 1
-  if (balls - share < share) {
-    for (i = balls; i > share; i--) {
-      top *= i
+// https://school.programmers.co.kr/learn/courses/30/lessons/181830
+function solution(array) {
+  let row = array.length
+  let col = array[0].length
+  let i, j
+  if (row < col) {
+    for (i = row; i < col; i++) {
+      array.push([0])
+      for (j = 0; j < col - 1; j++) array[i].push(0)
     }
-    for (i = balls - share; i > 0; i--) {
-      bot *= i
-    }
-  } else {
-    for (i = balls; i > balls - share; i--) {
-      top *= i
-    }
-    for (i = share; i > 0; i--) {
-      bot *= i
-    }
-  }
-  return top / bot
+  } else if (row > col)
+    for (i = 0; i < row; i++) for (j = col; j < row; j++) array[i].push(0)
+
+  return array
 }
+console.log(
+  solution([
+    [57, 192, 534, 2],
+    [9, 345, 192, 999],
+  ]),
+)
