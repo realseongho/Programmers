b597dc266a94c29b96a8eb75badfc912eb7bc9da
realseongho <qotjdgh000@gmail.com>
2024-06-26 19:56:37 +0900

    4

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120869
-function solution(spell, dic) {
-  let answer = 2
-  dic.forEach((data) => {
-    if (answer == 1) return answer
-    let sum = 0
-    spell.forEach((ch) => {
-      if (data.includes(ch)) sum++
-      if (sum >= spell.length) {
-        answer = 1
-        return
-      }
-    })
+// https://school.programmers.co.kr/learn/courses/30/lessons/181836
+function solution(picture, k) {
+  let picture2 = []
+  let count = 0
+  for (let i = 0; i < picture.length * k; i++) picture2.push('') // 배열에 필요한 인덱스까지 빈배열 할당
+  picture.forEach((pic, index) => {
+    count++
+    for (i = 0; i < pic.length; i++)
+      for (let j = 0; j < k; j++)
+        picture2[index * k] = picture2[index * k].concat(pic[i]) // 행*k
+    while (count < (index + 1) * k) picture2[count++] = picture2[index * k] // 열*k
   })
-  return answer
+  return picture2
 }
-console.log(
-  solution(['s', 'o', 'm', 'd'], ['moos', 'dzx', 'smm', 'sunmmo', 'som']),
-)
