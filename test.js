06a0863ff2724bee44a2a1fd6f3d57c5c7d5b86a
realseongho <qotjdgh000@gmail.com>
2024-07-02 19:24:59 +0900

    5

--- test.js
+++ test.js
@@ -1,14 +1,9 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181836
-function solution(picture, k) {
-  let picture2 = []
-  let count = 0
-  for (let i = 0; i < picture.length * k; i++) picture2.push('') // 배열에 필요한 인덱스까지 빈배열 할당
-  picture.forEach((pic, index) => {
-    count++
-    for (i = 0; i < pic.length; i++)
-      for (let j = 0; j < k; j++)
-        picture2[index * k] = picture2[index * k].concat(pic[i]) // 행*k
-    while (count < (index + 1) * k) picture2[count++] = picture2[index * k] // 열*k
-  })
-  return picture2
+// https://school.programmers.co.kr/learn/courses/30/lessons/181943
+function solution(my_string, overwrite_string, s) {
+  my_string = my_string.split('')
+  overwrite_string = overwrite_string.split('')
+  for (let i = s, j = 0; i < s + overwrite_string.length; i++, j++) {
+    my_string[i] = overwrite_string[j]
+  }
+  return my_string.join('')
 }
