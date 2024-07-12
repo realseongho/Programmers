518180b33313a946cafb4ffea4a92009ce355b39
realseongho <qotjdgh000@gmail.com>
2024-07-12 18:20:44 +0900

    7

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120860
-function solution(dots) {
-  for (let i = 1; i < 4; i++)
-    if (dots[i][0] != dots[0][0] && dots[i][1] != dots[0][1])
-      return Math.abs((dots[0][0] - dots[i][0]) * (dots[0][1] - dots[i][1]))
+// https://school.programmers.co.kr/learn/courses/30/lessons/120922
+function solution(M, N) {
+  return M * N - 1
 }
+console.log(solution(2, 5))
